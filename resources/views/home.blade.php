143eaa02579814804a2b319bfe33feaed9a81d5e
NicolaPlacentino <nyko0921@gmail.com>
2023-02-22 16:28:28 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Nicola Placentino">
+    <title>Hello World</title>
+</head>
+
+<body>
+    <h1>Hello World</h1>
+    <p>Da {{$name}}</p>
+</body>
+
+</html>
